0821f96f190dd5823e3a82364a9dcce8b894ec99
RajuKoushik <g.rajukoushik@gmail.com>
2018-11-08 00:27:06 -0700

    Updated the views.py with the url redirect view -using the sha1 hashing algorithm
    Signed-off-by: RajuKoushik <g.rajukoushik@gmail.com>

--- api/views.py
+++ api/views.py
@@ -13,4 +13,24 @@ import hashlib
 import random
 
 
+def home(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = DetailsForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            print(form.cleaned_data['username'])
 
+            print(int(hashlib.sha1(form.cleaned_data['username'].encode('utf-8')).hexdigest(), 16) % (10 ** 8))
+            hash = int(hashlib.sha1(form.cleaned_data['username'].encode('utf-8')).hexdigest(), 16) % (10 ** 8)
+
+            return HttpResponseRedirect('/' + str(hash) + '/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = DetailsForm()
+
+    return render(request, 'api/home.html', {'form': form})
